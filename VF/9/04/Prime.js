139ee5122c5b280f3980cd56be7fdf7ff9153a37
hirosphere <umoof.x@gmail.com>
2018-10-28 14:57:31 +0900

    18.10.28 E

--- VF/9/04/Prime.js
+++ VF/9/04/Prime.js
@@ -3,9 +3,18 @@ let 型を作成 = function( 典型装飾関数 )
 {
 	let 型 = function(){};
 
+	典型装飾関数.call( 型.prototype );
+
+	if( 型.prototype.開始する == undefined )
+	{
+		型.prototype.開始する = function(){};
+	}
+
 	型.作成 = function()
 	{
-		;
+		let 実体 = new 型();
+		型.prototype.開始する.apply( 実体, arguments );
+		return 実体;
 	};
 
 	return 型;
