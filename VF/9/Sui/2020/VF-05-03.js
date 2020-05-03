da5f33d01c91a4d4cd3bb94be939cef81d3786aa
hirosphere <umoof.x@gmail.com>
2020-05-04 01:14:24 +0900

    20. 05. 04. A

--- VF/9/Sui/2020/VF-05-03.js
+++ VF/9/Sui/2020/VF-05-03.js
@@ -1,38 +1,37 @@
 
-const VF = {};
-
-VF.Train = function()
+const VF = new function()
 {
+	{
+		const len = 128
+		const zero = len / 2;
+		const curve = new Float32Array( len );
+		for( let i = 0; i < len; i ++ )
+		{
+			curve[ i ] = ( i > zero ? 1 : - 1 );
+		}
+		this.PW_Shape_Curve_2 = curve;
+	}
 };
 
-VF.SoundGen = function()
+VF.SoundGen = function( doc )
 {
-	const context = new AudioContext();
 	this.Volume = 70;
 	this.IsPlay = false;
 
-	//
-
+	const context = new AudioContext();
 	const master_volume = context.createGain();
-	const test_oscillator = context.createOscillator();
-
-	//
 	master_volume.connect( context.destination );
-		test_oscillator.connect( master_volume );
+
+	const train = new VF.Train( doc, context );
+	const pwm = new VF.PWM( context, train.Power, master_volume, - 0.485, 0.495, VF.PW_Shape_Curve_2 );
+
 
 	//
-
-	test_oscillator.frequency.value = 300;
-	test_oscillator.type = "triangle";
-	test_oscillator.start();
-
-	//
-
-	const pwm = new VF.PWM( context );
 
 	this.Start = function()
 	{
 		this.IsPlay = true;
+		train.Start();
 		this.Update();
 	};
 
@@ -55,10 +54,58 @@ VF.SoundGen = function()
 	this.Update();
 };
 
-VF.PWM = function( context )
+VF.Train = function( doc, context )
 {
-	this.SetScadule = function()
+	const power = this.Power = context.createConstantSource();
+	power.offset.value = 0;
+	power.start();
+
+	// 
+
+	this.Start = function()
 	{
-		;
+		const schedule = [ [ 2, 0.05, 0.1 ], [ 15.0, 0.1, 100 ], [ 5, 100, 100 ], [ 15.0, 100, 0 ], [ 0, 0, 0 ] ];
+		let t = context.currentTime + 0.001;
+
+		power.offset.cancelAndHoldAtTime( t );
+
+		for( let i in schedule )
+		{
+			const item = schedule[ i ];
+
+			const time = item[ 0 ] || 0;
+			const start_v = item[ 1 ] / 100;
+			const end_v = item[ 2 ] / 100;
+
+			console.log( t, time, start_v, end_v );
+
+			power.offset.setValueAtTime( start_v, t );
+			power.offset.linearRampToValueAtTime( end_v, t + time );
+
+			t += time;
+		}
 	};
+};
+
+VF.PWM = function( context, power, output, offset_v, att_v, shape_curve )
+{
+	const carrier = context.createOscillator();
+	const att = context.createGain();
+	const offset = context.createConstantSource();
+	const shaper = context.createWaveShaper();
+	
+	carrier.frequency.value = 300 / 1;
+	carrier.type = "triangle";
+	att.gain.value = att_v;
+	offset.offset.value = offset_v;
+	shaper.curve = shape_curve;	
+
+	carrier.connect( att );
+	att.connect( shaper );
+	offset.connect( shaper );
+	power.connect( shaper );
+	shaper.connect( output );
+
+	carrier.start();
+	offset.start();
 };
