42dd794dffa7ea8471bab2e951df7578f607b44d
hirosphere <umoof.x@gmail.com>
2020-05-05 21:54:09 +0900

    20. 05. 05. A

--- VF/9/Sui/2020/VF-05-03.js
+++ VF/9/Sui/2020/VF-05-03.js
@@ -1,22 +1,11 @@
 
 const VF = new function()
 {
-	{
-		const len = 128
-		const zero = len / 2;
-		const curve = new Float32Array( len );
-		for( let i = 0; i < len; i ++ )
-		{
-			curve[ i ] = ( i > zero ? 1 : - 1 );
-		}
-		this.PW_Shape_Curve_2 = curve;
-	}
 };
 
 VF.SoundGen = function( doc )
 {
 	this.Volume = 30;
-	this.IsPlay = false;
 
 	const context = new AudioContext();
 	const master_volume = context.createGain();
@@ -27,80 +16,46 @@ VF.SoundGen = function( doc )
 	eq_1.connect( master_volume );
 
 	const train = new VF.Train( doc, context );
-	const pwm = new VF.PWM( context, train.Power, train.Freq, eq_1, - 0.485, 0.495, VF.PW_Shape_Curve_2 );
-
+	const dc_pwm = new VF.DCPWM( context, train, eq_1 );
 
 	//
 
 	this.Start = function()
 	{
-		this.IsPlay = true;
-		train.Start();
+		const time = context.currentTime + 0.01;
+		dc_pwm.Start( time, doc.キャリア設定 );
+		train.Start( time );
 		this.Update();
 	};
 
 	this.Stop = function()
 	{
-		this.IsPlay = false;
-		this.Update();
+		const time = context.currentTime + 0.01;
+		dc_pwm.Stop( time );
+		train.Stop( time );
 	};
 	
 	this.Update = function()
 	{
-		master_volume.gain.value = this.IsPlay ? this.Volume / 100 : 0;
+		master_volume.gain.value = this.Volume / 100;
 		eq_1.frequency.value = doc.音響1.周波数;
 		eq_1.Q.value = doc.音響1.共振;
-	};
-
-	this.Play = function( doc, vf )
-	{
-		;
 	};
 };
 
 VF.Train = function( doc, context )
 {
-	let carrier_def = null;
-	
 	const power = this.Power = context.createConstantSource();
 	power.offset.value = 0;
 	power.start();
 
-	const freq = this.Freq = context.createScriptProcessor( 0, 1, 1 );
-
-	freq.onaudioprocess = ( ev ) =>
-	{
-		const input = ev.inputBuffer.getChannelData( 0 );
-		const output = ev.outputBuffer.getChannelData( 0 );
-		const freq = ptof( input[ 0 ] * 100 );
-
-		for( let i = 0; i < output.length; i ++ )
-		{
-			output[ i ] = freq;
-		}
-	};
-
-	const ptof = ( power ) =>
-	{
-		//return 600;
-		let freq = 0;
-		if( carrier_def ) for( let item of carrier_def )
-		{
-			freq = item[ 1 ];
-			if( power < item[ 0 ] ) break;
-		}
-		return freq;
-	};
-
-	power.connect( freq );
-
 	// 
 
-	this.Start = function()
+	this.Start = function( t )
 	{
-		const schedule = doc.運転;
 		context.resume();
-		let t = context.currentTime + 0.001;
+
+		const schedule = doc.運転;
 
 		power.offset.cancelAndHoldAtTime( t );
 
@@ -112,39 +67,102 @@ VF.Train = function( doc, context )
 			const start_v = item[ 1 ] / 100;
 			const end_v = item[ 2 ] / 100;
 
-			console.log( t, time, start_v, end_v );
-
 			power.offset.setValueAtTime( start_v, t );
 			power.offset.linearRampToValueAtTime( end_v, t + time );
 
 			t += time;
 		}
+	};
 
-		carrier_def = doc.キャリア設定;
-		if( carrier_def == null || carrier_def.length == 0 )  carrier_def = [ 100, 300 ];
+	this.Stop = function( time )
+	{
+		power.offset.cancelAndHoldAtTime( time );
 	};
 };
 
-VF.PWM = function( context, power, freq, output, offset_v, att_v, shape_curve )
+VF.DCPWM = function( context, train, destination )
 {
-	const carrier = context.createOscillator();
-	const att = context.createGain();
-	const offset = context.createConstantSource();
-	const shaper = context.createWaveShaper();
+	// インターフェース //
+
+	this.Start = ( time, carrier_def_ ) =>
+	{
+		carrier_def =
+		(
+			carrier_def_ && carrier_def_.constructor == Array && carrier_def_.length > 0 ?
+
+				carrier_def_ : [ [ 1.5, 150, "C" ], [ 3.0, 300, "C" ], [ 4.5, 600, "C" ], [ 6, 900, "C" ],  ]
+		);
+		enable_sig.offset.setValueAtTime( 1, time );
+	};
+
+	this.Stop = ( time ) => enable_sig.offset.setValueAtTime( 0, time );
+
+	// 実装 //
+
+	const enable_sig = context.createConstantSource();
+	const carrier = context.createScriptProcessor( 0, 2, 1 );
+	let phase = 0;
+	let carrier_def;
+
+	const ptof = ( power ) =>
+	{
+		let freq = 0;
+		if( carrier_def ) for( let item of carrier_def )
+		{
+			freq = item[ 1 ] || 0;
+			const point = item[ 0 ] || 0;
+
+			if( power < ( point ) ) break;
+		}
+		return freq;
+	};
+
+	carrier.onaudioprocess = ( ev ) =>
+	{
+		const power_in = ev.inputBuffer.getChannelData( 0 );
+		const enable_in = ev.inputBuffer.getChannelData( 1 );
+		const output = ev.outputBuffer.getChannelData( 0 );
+
+		for( let i = 0; i < output.length; i ++ )
+		{
+			if( ! enable_in[ i ] )
+			{
+				output[ i ] = 0;
+				continue;
+			}
+
+			const power = power_in[ i ];
+			const freq = ptof( power * 100 );
+			const step = freq / context.sampleRate;
+
+			output[ i ] = get_pulse( power, phase, step );
+			//output[ i ] = ( ( power + phase ) > 1 ? - 1 : 1 );
+
+			if( ( phase += step ) >= 1 ) phase -= 1;
+		}
+	};
+
+	const get_pulse = ( power, phase, step ) =>
+	{
+		const saw = phase;
+		const tri = 2 * ( phase < 0.5 ? phase : 1 - phase );
+
+		const carr = ( step +  tri * ( 1 - step ) );
+		const gain = 1 / step;
+		const wf = Math.max( 0, carr - 1 + power ) * gain;
+		return Math.min( 2, wf * 2 ) - 1;
+	};
+
+	//- 結線 //
+
+	const merger = context.createChannelMerger( 2 );
+	{
+		train.Power.connect( merger, 0, 0 );
+		enable_sig.connect( merger, 0, 1 );
+		merger.connect( carrier );
+	}	
+	carrier.connect( destination );
 	
-	carrier.frequency.value = 0;
-	freq.connect( carrier.frequency );
-	carrier.type = "triangle";
-	att.gain.value = att_v;
-	offset.offset.value = offset_v;
-	shaper.curve = shape_curve;	
-
-	carrier.connect( att );
-	att.connect( shaper );
-	offset.connect( shaper );
-	power.connect( shaper );
-	shaper.connect( output );
-
-	carrier.start();
-	offset.start();
-};
+	enable_sig.offset.value = 0;
+	enable_sig.start();
+} ;
