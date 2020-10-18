d1977944ebcaadb31616c28210bf50c666ef9e3f
hirosphere <umoof.x@gmail.com>
2020-10-18 13:40:21 +0900

    20. 10. 18. B

--- VF/9/Sui/2020/10-17-Touch/Keyboard.js
+++ VF/9/Sui/2020/10-17-Touch/Keyboard.js
@@ -2,72 +2,25 @@
 class Keyboard
 {
 	KeyPitch = 45;
-	constructor( com )
+	Channel = 0;
+
+	constructor( com, on_event )
 	{
+		this.on_event = on_event;
+
 		const e = this.e = ecr( "div", com, { class: "KB" } );
+		this.kb_con = e;
 		this.mon = ecr( "div", com, { style: { height: "20px" } } );
 
 		this.touch_works = {};
 		this.note_id = 0;
 		this.key_buttons = {};
-		this.create_key_buttons( 0, 25 );
+		this.create_key_buttons( 52, 77 );
 
 		e.addEventListener( "touchstart", ev => this.touchstart( ev ) );
 		e.addEventListener( "touchmove", ev => this.touchmouve( ev ) );
 		e.addEventListener( "touchend", ev => this.touchend( ev ) );
 		e.addEventListener( "touchcancel", ev => this.touchend( ev ) );
-	}
-
-	// touch //
-
-	touchstart( ev )
-	{
-		//if( ! ev )  return;
-		ev.preventDefault();
-		for( const touch of ev.changedTouches )
-		{
-			const w = this.get_touch_work( touch );
-			const key = this.touch_to_key( touch );
-			if( key == w.key )  break;
-			w.key = key;
-			this.post_note_on( w )
-		}
-	}
-
-	touchmouve( ev )
-	{
-		if( ! ev )  return;
-		for( const touch of ev.changedTouches )
-		{
-			const w = this.get_touch_work( touch );
-			const key = this.touch_to_key( touch );
-			if( key == w.key )  break;
-			this.post_note_off( w );
-			w.key = key;
-			this.post_note_on( w );
-		}
-	}
-
-	touchend( ev )
-	{
-		if( ! ev )  return;
-		for( const touch of ev.changedTouches )
-		{
-			const w = this.get_touch_work( touch );
-			this.post_note_off( w );
-			w.key = undefined;
-		}
-	}
-
-	touch_to_key( ev )
-	{
-		return this.pos_to_key(  ev.pageX - this.e.offsetLeft, ev.pageY - this.e.offsetTop);
-	}
-
-	get_touch_work( touch )
-	{
-		const id = touch.identifier;
-		return this.touch_works[ id ] || ( this.touch_works[ id ] = { id: id } );
 	}
 
 	// pos //
@@ -96,7 +49,7 @@ class Keyboard
 	create_key_button( left, label, key )
 	{
 		const top = 0 + "px";
-		const e = ecr( "span", this.e, { class: "KI", text: label, style: { left: left + "px", top: top } } );
+		const e = ecr( "span", this.kb_con, { class: "KI", text: label, style: { left: left + "px", top: top } } );
 		e.active = 0;
 		e.add_active = ( v ) =>
 		{
@@ -106,6 +59,65 @@ class Keyboard
 		this.key_buttons[ key ] = e;
 	}
 
+	// touch //
+
+	touchstart( ev )
+	{
+		ev.preventDefault();
+		for( const touch of ev.changedTouches )
+		{
+			if( ! this.is_key_button( touch.target ) ) continue;
+
+			const w = this.get_touch_work( touch );
+			w.key = this.touch_to_key( touch );
+			this.post_note_on( w )
+		}
+	}
+
+	touchmouve( ev )
+	{
+		for( const touch of ev.changedTouches )
+		{
+			if( ! this.is_key_button( touch.target ) ) continue;
+			
+			const w = this.get_touch_work( touch );
+			const key = this.touch_to_key( touch );
+			if( key == w.key )  continue;
+			
+			this.post_note_off( w );
+			w.key = key;
+			this.post_note_on( w );
+		}
+	}
+
+	touchend( ev )
+	{
+		for( const touch of ev.changedTouches )
+		{
+			if( ! this.is_key_button( touch.target ) ) continue;
+
+			const w = this.get_touch_work( touch );
+			this.post_note_off( w );
+			w.key = undefined;
+		}
+	}
+
+	touch_to_key( ev )
+	{
+		return this.pos_to_key(  ev.pageX - this.e.offsetLeft, ev.pageY - this.e.offsetTop);
+	}
+
+	get_touch_work( touch )
+	{
+		const id = touch.identifier;
+		return this.touch_works[ id ] || ( this.touch_works[ id ] = { id: id } );
+	}
+
+	is_key_button( e )
+	{
+		return e != null && e.parentNode == this.kb_con;
+	}
+
 	// note //
 
 	post_note_on( w )
@@ -113,6 +125,8 @@ class Keyboard
 		const note_id = w.note_id = ++ this.note_id;
 		const kb = this.key_buttons[ w.key ];
 		kb && kb.add_active( 1 );
+		this.on_event( { type: "Key-On", ch: this.Channel, note_id: w.note_id, key: w.key, touch_id: w.id } );
+		
 		this.post( [ "Key-On", "w" + w.id, "n" + w.note_id, "k" + w.key ].join( " " ) );
 	}
 
@@ -120,6 +134,8 @@ class Keyboard
 	{
 		const kb = this.key_buttons[ w.key ];
 		kb && kb.add_active( -1 );
+		this.on_event( { type: "Key-Off", ch: this.Channel, note_id: w.note_id, key: w.key, touch_id: w.id } );
+		
 		this.post( [ "Key-Off", "w" + w.id, "n" + w.note_id, "k" + w.key ].join( " " ) );
 	}
 
---
+++ VF/9/Sui/2020/10-17-Touch/SoundGen.js
@@ -0,0 +1,66 @@
+
+class SoundGen
+{
+	volume = 10;
+	voices = {};
+
+	constructor()
+	{
+		const c = this.context = new AudioContext();
+		const g = this.voice_dest = c.createGain( { gain: 0 } );
+		g.connect( c.destination );
+		this.Update();
+	}
+
+	Update()
+	{
+		this.voice_dest.gain.value = this.volume / 100;
+	}
+
+	PostKeyEvent( ev )
+	{
+		if( ev.type == "Key-On" )
+		{
+			this.voices[ ev.note_id ] = new Voice( this.context, this.voice_dest, ev.key );
+		}
+		else if( ev.type == "Key-Off" )
+		{
+			const voice = this.voices[ ev.note_id ];
+			if( voice == null ) return;
+
+			voice.release();
+			delete this.voices[ ev.note_id ];
+		}
+	}
+}
+
+class Voice
+{
+	constructor( context, dest, key )
+	{
+		this.context = context;
+
+		const o = this.osc = context.createOscillator();
+		const g = this.gain = new GainNode( context, { gain: 0 } );
+		
+		o.connect( g );
+		g.connect( dest );
+		
+		o.detune.value = key * 100;
+
+		const t = context.currentTime + 0.001;
+		console.log( t );
+		o.start( t );
+		g.gain.setTargetAtTime( 1 / 0.63, t, 0.1 );
+	}
+
+	release()
+	{
+		const t = this.context.currentTime + 0.001;
+		const t_r = t + 5.5;
+		this.gain.gain.cancelAndHoldAtTime( t );
+		this.gain.gain.setTargetAtTime( 0, t, 0.5 );
+		this.osc.stop( t_r );
+		this.osc = undefined;
+	}
+}
